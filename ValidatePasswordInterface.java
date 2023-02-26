c4f78f837f6564d084edb294057bc423e40793f3
ctn <35670719+nguyenchloet@users.noreply.github.com>
2023-02-26 10:02:30 -0500

    Create ValidatePasswordInterface.java

---
+++ ValidatePasswordInterface.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Password {
+	public static void main(String[] args) {
+		//user input password
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("(Password should be at least 6 characters long, \n contain at least one uppercase \n and one lowercase letter and one digit)\n");
+     	System.out.println("Please enter password: ");
+     	String inputPass = scanner.nextLine();
+     	
+    if (!ValidatePassword.validatePassword(inputPass)) {
+        System.out.println("Invalid Password");
+        System.out.print("Please Re-enter Password: ");
+        inputPass = scanner.nextLine();
+    } else {
+      System.out.println("Valid input. Thank you.");
+    } 
+  }
+}
